99d976426686340e61939cd917aeb86762b1db0c
mo01012 <115111548+mo01012@users.noreply.github.com>
2022-11-25 11:32:51 +0200

    00

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(".after").mouseenter(function() {
-        $(".none").removeClass("d-none");
-    })
-    $(".after").mouseleave(function() {
-        $(".none").addClass("d-none");
-    })
-})
