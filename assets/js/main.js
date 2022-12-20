f6b593321479ff91891ae352c4bebcf8aa3305e9
mo01012 <115111548+mo01012@users.noreply.github.com>
2022-12-20 22:42:30 +0200

    0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,35 @@
 $(document).ready(function() {
+    $("aside").hide();
     $(".link").click(function(e) {
         e.preventDefault();
         $("aside").slideToggle("slow");
     })
-    var x = $(document).height();
-    // $("aside").after().css("height", x);
+
     $("aside").after().click(function() {
         $("aside").slideUp("slow");
     })
-    console.log(x)
+    $('.owl-carousel').owlCarousel({
+        rtl: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+    $(".hover").mouseenter(function() {
+        $(this).children("p").removeClass("d-none");
+    })
+    $(".hover").mouseleave(function() {
+        $(this).children("p").addClass("d-none");
+    })
 });
